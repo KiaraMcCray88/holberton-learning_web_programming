b186ff6a8f1f263cd3cb2b0d6cf684d326736505
Kiara <kmccray866@gmail.com>
2019-08-02 13:32:03 -0400

    Update 0-hello_world.js

--- 5-javascript/0-hello_world.js
+++ 5-javascript/0-hello_world.js
@@ -1,15 +1,1 @@
-<!DOCTYPE hmtl>
-<html>
-<body>
-
-<h1> My first web page</h1>
-<p> my first parargraph</p>
-
-<p id "demo"></p>
-
-<script>
-
-</script>
-
-</body>
-</hmtl>
+console.log("Betty Holberton")
