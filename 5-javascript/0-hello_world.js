744a121d723973acd1292884b79065ee629d24a9
Kiara <kmccray866@gmail.com>
2019-08-02 13:43:15 -0400

    Update 0-hello_world.js

--- 5-javascript/0-hello_world.js
+++ 5-javascript/0-hello_world.js
@@ -1,11 +1,11 @@
 console.log("Betty Holberton")
-2+2
-4
-1+2
-3
-1-2
--1
-1*2
-2
-1/2
-0.5
+let x=2
+undefined
+console.log(x)
+VM1453:1 2
+undefined
+let variable_test="Holberton"
+undefined
+console.log(variable_test)
+VM1623:1 Holberton
+undefined
